941b5eed9b12ee65ebe53bd1358f255e5fd30fd1
Kapilam <Kapilam@users.noreply.github.com>
2018-06-24 04:19:26 +0100

    URL link added

--- Tutorial1-Helloworld/Tutorial1-Helloworld/routes.py
+++ Tutorial1-Helloworld/Tutorial1-Helloworld/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask;
+from flask import Flask, url_for;
 from app import app;
 
 
@@ -6,14 +6,16 @@ from app import app;
 @app.route('/')
 def hello():
     """Renders a sample page."""
+    createLink = "<a href='" + url_for('create') + "'>Create a question</a>";
     return """<html>
                     <head>
                         <title> Hello, world </title>
                      </head>
                      <body>
+                            """ + createLink + """
                         <h1> Hello World KM </h1>
-                        <h2> header 1 </h2>
-                        <h3> header 3</h3> 
+                        <h2> type "/question/km in url" </h2>
+                        <h3> you will see km page</h3> 
                     <body>
               </html>"""
 # Server/create
@@ -23,4 +25,4 @@ def create():
 # server/question<title>
 @app.route('/question/<title>')
 def question(title):
-    return '<h2>' + title + '<h2>';
+    return '<h2>' + title + '</h2>';
