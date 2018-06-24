66d94e99920f305a41d61735a11555faa8bc8023
Kapilam <Kapilam@users.noreply.github.com>
2018-06-24 02:02:14 +0100

    header 3 added

--- Tutorial1-Helloworld/Tutorial1-Helloworld/app.py
+++ Tutorial1-Helloworld/Tutorial1-Helloworld/app.py
@@ -21,7 +21,8 @@ def hello():
                      </head>
                      <body>
                         <h1> Hello World KM </h1>
-                        <h2> header 1 <h2>
+                        <h2> header 1 </h2>
+                        <h3> header 3</h3> 
                     <body>
               </html>"""
 
